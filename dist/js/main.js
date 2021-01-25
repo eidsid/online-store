d07556e628f2c7f056dc77212a13ce2846fde5f1
eid sayed <eidsiddiedis@gmail.com>
2021-01-25 14:57:41 +0200

    add product info page

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,11 +18,11 @@ class cart {
             //      category.text(category)
             let productinfo_container = $('.productinfo-container .product');
             productinfo_container.attr('id', id);
-            productinfo_container.children('.header').children('.title').text(title);
-            productinfo_container.children('.body ').children('.info').text(info);
-            productinfo_container.children('.body').children('.img').attr('src', imgurl);
-            productinfo_container.children('.footer').children('.price').text(price);
-            productinfo_container.children('.footer').children('.count').text(1);
+            productinfo_container.children('.prod-header').children('.title').text(title);
+            productinfo_container.children('.prod-body ').children('.info').text(info);
+            productinfo_container.children('.prod-body').children('.img').attr('src', imgurl);
+            productinfo_container.children('.prod-footer').children('.price').text(price);
+            productinfo_container.children('.prod-footer').children('.count').text(1);
 
         }
         // api product info data by id
